b0247ac9cd2efa6db4dbed70e93bdf5ada7a8b51
afdalabdallahm <afdalabdallahm@gmail.com>
2021-10-04 03:32:23 +0700

    add router pack

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function routes() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default routes
--- src/sections/Navbar.js
+++ src/sections/Navbar.js
@@ -1,10 +1,13 @@
-import { Box } from '@chakra-ui/layout'
-import React from 'react'
+import { Box, Text } from '@chakra-ui/layout'
+import React, {useRef} from 'react'
+import {scroller} from 'react-scroll'
 
 function Navbar() {
+    
+
     return (
         <Box height="109" bgColor="#EBEDFF" className="navbar">
-
+            <Text cursor="pointer">Artikel</Text>
         </Box>
     )
 }
