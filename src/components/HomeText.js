8e300be65e1532e7defd2942da4605e01210f171
Afdal-Abdallah <afdalabdallah236755@gmail.com>
2021-10-06 16:43:16 +0700

    dikit perbaiki

--- src/components/HomeText.js
+++ src/components/HomeText.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import {Grid, GridItem} from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
 export default function HomeText() {
     return (
         <Grid
         w="80%"
-        h="350px"
-        templateRows="repeat(10, 1fr)"
-        templateColumns="repeat(4, 1fr)"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(2, 1fr)"
         gap={2}
         margin="auto"
-        marginTop="5%"
+        marginTop="1%"
         fontFamily="Montserrat"
         color="#545DCA"
             >
-            <GridItem rowSpan={1.5} colSpan={2} fontSize="64pt" fontWeight="700">Data Terkini</GridItem>
-            <GridItem marginTop="5%" rowSpan={3} colSpan={2} fontSize="60.78pt" fontWeight="700" color="#FFF" borderRadius="100px" bgColor="#6670DF" textAlign="center">2.123.013 kasus</GridItem>
-            <GridItem rowSpan={4} colSpan={2} fontSize="21.33pt">Jumlah kasus penyebaran Covid - 19 di <br/>Indonesia sampai saat ini</GridItem>
-            <GridItem rowSpan={2} colSpan={2}></GridItem> {/*kosong untuk spacing bawah*/}
-            <GridItem rowSpan={2} colSpan={2} fontSize="13.48pt" fontStyle="italic">23 Septermber 2021</GridItem>
+            <GridItem rowSpan={1} colSpan={1} >
+                <Text fontSize="64px" fontWeight="700">
+                    Data Terkini
+                </Text>
+            </GridItem>
+            <GridItem marginTop="3%" rowSpan={2} colSpan={1}   borderRadius="100px" bgColor="#6670DF" textAlign="center" w="572px" h="103px">
+               <Text marginTop="4px" fontSize="60.78px" fontWeight="bold" color="#FFF">
+                   2.123.013 kasus
+                </Text>
+            </GridItem>
+            <GridItem rowSpan={1} colSpan={1} fontSize="21.33px">
+                Jumlah kasus penyebaran Covid - 19 di 
+                <br/>
+                Indonesia sampai saat ini
+                <br/>
+                <Text marginTop="8px" fontSize="13.48px" fontStyle="italic">23 Septermber 2021</Text>
+            </GridItem>
         </Grid>
     )
 }
