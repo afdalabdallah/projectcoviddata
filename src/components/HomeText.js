7007188528ded5d5d4f645d1c803a42325e3a8c6
Fayyadh Hafizh <you@example.com>
2021-10-07 19:07:45 +0700

    adding article boxes

--- src/components/HomeText.js
+++ src/components/HomeText.js
@@ -5,7 +5,6 @@ import { Text } from '@chakra-ui/react'
 export default function HomeText() {
     return (
         <Grid
-<<<<<<< HEAD
         w="80%"
         templateRows="repeat(2, 1fr)"
         templateColumns="repeat(2, 1fr)"
@@ -32,23 +31,6 @@ export default function HomeText() {
                 <br/>
                 <Text marginTop="8px" fontSize="13.48px" fontStyle="italic">23 Septermber 2021</Text>
             </GridItem>
-=======
-        w="90%"
-        h="350px"
-        templateRows="repeat(10, 1fr)"
-        templateColumns="repeat(8, 1fr)"
-        gap={2}
-        marginLeft="auto"
-        marginTop="5%"
-        fontFamily="Montserrat"
-        color="#545DCA"
-            >
-            <GridItem rowSpan={2} colSpan={4} fontSize="64px" fontWeight="700">Data Terkini</GridItem>
-            <GridItem marginLeft="5%" marginTop="5%" rowSpan={4} colStart={5} colEnd={8} fontSize="60.78px" fontWeight="700" color="#FFF" borderRadius="100px" bgColor="#6670DF" textAlign="center">2.123.013 kasus</GridItem>
-            <GridItem rowStart={4} rowEnd={6}colSpan={3} fontSize="21.33pt">Jumlah kasus penyebaran Covid - 19 di <br/>Indonesia sampai saat ini</GridItem>
-            <GridItem rowSpan={2} colSpan={2}></GridItem> {/*kosong untuk spacing bawah*/}
-            <GridItem rowStart={7} colSpan={2} fontSize="13.48pt" fontStyle="italic">23 Septermber 2021</GridItem>
->>>>>>> 49692c7c410f56995eee8604cafb5c6401a5f7bb
         </Grid>
     )
 }
--- src/sections/Artikel.js
+++ src/sections/Artikel.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Flex, IconButton } from '@chakra-ui/react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import ArticleBox from '../components/ArticleBox'
 
-function Artikel() {
-    return (
-        <Box id="artikel" height="400px" bgColor="#FFD6C3" marginBottom="200"> 
-            
-        </Box>
-    )
+function Artikel({text, id}) {
+    const [items, setItems] = useState([]);
+
+    var text = "Hebat! Indonesia";
+    var idx = 1;
+
+    for(var i = 0; i < 4; i++){
+        items[i] = {
+            text: {text}
+        }
+    }
+
+    return ( 
+        <Flex position="relative" id="artikel" height="400px" bgColor="#FFD6C3" marginBottom="200" zIndex="-1">
+            <Flex w="100%" mt={5} mb={5}>
+                <IconButton ml="4px" margin="auto" borderRadius="50px" icon={<FaChevronLeft color="#FFFFFF" size={75}/>} bgColor="black" width="100px" height="100px"/>
+                {items.map(() => (
+                    <ArticleBox
+                        text={text}
+                        id={idx++}
+                    />
+                ))}
+                <IconButton ml="50px" margin="auto" borderRadius="50px" icon={<FaChevronRight color="#FFFFFF" size={75}/>} bgColor="black" width="100px" height="100px"/>
+            </Flex>
+        </Flex>
+    );
 }
 
 export default Artikel
